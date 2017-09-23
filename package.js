2e6e83f6ba216b6f744485df934c1f7f631637e0
chitai <chitai.vct@gmail.com>
2017-09-23 11:59:27 +0700

    Fix UI

--- package.js
+++ package.js
@@ -1,27 +1,42 @@
 Package.describe({
-    name: 'vchitai:autoform-ckeditor',
-    version: '0.0.1',
-    // Brief, one-line summary of the package.
-    summary: 'CkEditor for aldeed:autoform',
-    // URL to the Git repository containing the source code for this package.
-    git: 'https://github.com/vchitai/meteor-autoform-ckeditor',
-    // By default, Meteor will default to using README.md for documentation.
-    // To avoid submitting documentation, set this field to null.
-    documentation: 'README.md'
+  name: 'vchitai:autoform-mathanswerbox',
+  version: '0.0.4',
+  // Brief, one-line summary of the package.
+  summary: 'Fast Math Answer Box for aldeed:autoform',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/vchitai/meteor-autoform-mathanswerbox',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-    api.versionsFrom('1.5.1');
-    api.use('ecmascript');
+  api.versionsFrom('1.5.1');
+  api.use('ecmascript');
     api.use([
-        'templating',
+        'templating@1.3.1',
         'jquery',
-        'reactive-var',
-        'aldeed:autoform',
+        'aldeed:autoform@5.8.1',
     ], 'client');
 
     api.addFiles([
-        'client/afCkEditor.html',
-        'client/afCkEditor.js',
+        'client/afMathText.html',
+        'client/afMathText.js',
+        'client/mathquill.css',
+        'client/mathquill.js',
+       
     ], 'client');
+
+    api.addAssets([
+       'client/font/Symbola.eot',
+       'client/font/Symbola.otf',
+       'client/font/Symbola.svg',
+       'client/font/Symbola.ttf',
+       'client/font/Symbola.woff',
+       'client/font/Symbola.woff2',
+       'client/font/Symbola-basic.eot',
+       'client/font/Symbola-basic.ttf',
+       'client/font/Symbola-basic.woff',
+       'client/font/Symbola-basic.woff2',
+      ],'client');
 });
